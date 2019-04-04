3dc626cdbe3ae88f81b48d9e5e0d9973cee3b096
AP Keerthika <keerthi2592@gmail.com>
2019-04-04 16:51:37 +0530

    Add elevator button

--- components/ElevatorButtons.vue
+++ components/ElevatorButtons.vue
@@ -1,5 +1,6 @@
 <template lang="pug">
-h1 ElevatorButton {{ floors }}
+.elevator-buttons
+  a.btn(:class="{highlighted: currentFloor == i}", v-for="(f, i) in floors") {{ i }}
 </template>
 
 <script>
@@ -9,7 +10,7 @@ export default {
   },
   data () {
     return {
-
+      currentFloor: 0
     }
   },
   methods: {
@@ -23,5 +24,14 @@ export default {
 
 <style lang="sass" scoped>
 // @import 'assets/styles/includes'
+
+
+.btn
+  margin: 0.2rem
+  padding: 0.5rem 1rem
+  display: inline-block
+  border: 1px solid #f4f5f7
+  &.highlighted
+    border: 1px solid red
 
 </style>
